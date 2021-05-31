c9fe6023f010dc975642f599df99fba7e2809af8
gauravmalaviya143 <53636643+gauravmalaviya143@users.noreply.github.com>
2021-05-31 21:08:43 +0530

    Add files via upload

---
+++ nestedif.py
@@ -0,0 +1,9 @@
+x = int(input("enter the number:"))
+if ( x % 2 == 0):
+    print("even")
+    if (x > 5):
+        print("greater")
+    else:
+        print("not a greater")
+else:
+    print("odd")
---
+++ posneg.py
@@ -0,0 +1,5 @@
+x = float(input("enter the number:"))
+if(x>=0):
+    print("positive number")
+else:
+    print("negative number")
